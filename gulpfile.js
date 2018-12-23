46ca86fb5a494d227ed61d8f503c361ad0de9fe2
Cody Reed <cody2j2reed@gmail.com>
2018-12-22 22:19:11 -0500

    installed gulp-rename, and added to devDependiences. Created function minifyScripts in gulpfile.js to minify app.js into app.min.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 "use strict";
 
 const gulp = require("gulp"),
-  concat = require("gulp-concat");
+  concat = require("gulp-concat"),
+  uglify = require("gulp-uglify"),
+  rename = require("gulp-rename");
 
 function concatScripts(cb) {
   gulp
     .src(["js/jquery.js", "js/sticky/jquery.sticky.js", "js/main.js"])
     .pipe(concat("app.js"))
+    .pipe(gulp.dest("js"));
+  cb();
+}
+
+function minifyScripts(cb) {
+  gulp
+    .src("js/app.js")
+    .pipe(uglify())
+    .pipe(rename("app.min.js"))
     .pipe(gulp.dest("js"));
   cb();
 }
@@ -18,3 +29,4 @@ function goodbye(cb) {
 
 exports.default = goodbye;
 exports.concat = concatScripts;
+exports.minify = minifyScripts;
