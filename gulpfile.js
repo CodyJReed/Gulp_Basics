3e40177491c0d1452966e0eea4e0f27e89fd9015
Cody Reed <cody2j2reed@gmail.com>
2018-12-22 21:58:47 -0500

    Added concatScripts function to gulpfile.js Created app.js with concatScripts();

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
 "use strict";
 
-const { series } = require("gulp");
+const gulp = require("gulp"),
+  concat = require("gulp-concat");
 
-function hello(cb) {
-  console.log("Hello!");
+function concatScripts(cb) {
+  gulp
+    .src(["js/jquery.js", "js/sticky/jquery.sticky.js", "js/main.js"])
+    .pipe(concat("app.js"))
+    .pipe(gulp.dest("js"));
   cb();
 }
 
 function goodbye(cb) {
-  console.log("Goodbye!");
+  console.log("");
   cb();
 }
 
-exports.default = series(hello, goodbye);
+exports.default = goodbye;
+exports.concat = concatScripts;
