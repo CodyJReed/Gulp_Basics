5611c5f0e56db6a40bb34f669e70f09bcceeb8d9
Cody Reed <cody2j2reed@gmail.com>
2018-12-24 15:45:47 -0500

    Updated .gitignore/gulpfile.js
    Added .DS_Store to ignore file.
    
    Replaced error-callback in all "gulp task" functions with return/promise keyword.  Added default to exports = series(parallel())

--- gulpfile.js
+++ gulpfile.js
@@ -8,44 +8,39 @@ const gulp = require("gulp"),
   maps = require("gulp-sourcemaps");
 
 // Concat js files in js folder, and create new app.js, and ...map file. place into js folder
-function concatScripts(cb) {
-  gulp
+function concatScripts() {
+  return gulp
     .src(["js/jquery.js", "js/sticky/jquery.sticky.js", "js/main.js"])
     .pipe(maps.init())
     .pipe(concat("app.js"))
     .pipe(maps.write("./"))
     .pipe(gulp.dest("js"));
-  cb();
 }
 
 // Take app.js files and create new minified app.min.js file. place into js folder
-function minifyScripts(cb) {
-  gulp
+function minifyScripts() {
+  return gulp
     .src("js/app.js")
     .pipe(uglify())
     .pipe(rename("app.min.js"))
     .pipe(gulp.dest("js"));
-  cb();
 }
 
 // Take application.scss and compile with gulp-sass/sourcemaps.
 // Create both a application.css and ...map file(s) and place in css folder
-function compileSass(cb) {
-  gulp
+function compileSass() {
+  return gulp
     .src("scss/application.scss")
     .pipe(maps.init())
     .pipe(sass())
     .pipe(maps.write("./"))
     .pipe(gulp.dest("css"));
-  cb();
 }
 
-function goodbye(cb) {
-  console.log("");
-  cb();
-}
-
-exports.default = goodbye;
+exports.default = gulp.series(
+  gulp.parallel(concatScripts, compileSass),
+  minifyScripts
+);
 exports.concat = concatScripts;
 exports.minify = minifyScripts;
 exports.compile = compileSass;
